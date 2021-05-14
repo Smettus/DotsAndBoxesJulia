a30601dbaa556ff100c4e3cc21fbe44d21234928
Smettus <68818239+Smettus@users.noreply.github.com>
2021-05-14 15:38:33 +0200

    Fixed: name of box in middle + cool settings
    Needed: libraries CSV, DataFrames
    -> Startup time way slower.
    But now possible to change on the fly, from csv file in same folder

--- DotsAndBoxesAfterDeadline.jl
+++ DotsAndBoxesAfterDeadline.jl
@@ -16,14 +16,17 @@
 		- Output of score/game overview to text file
 		- Cool visuals, interface design
 """
-#######################<---Startup in REPL--->#######################
 #using BenchmarkTools
 #using CPUTime
 #using GameZero
 #using Colors
+using CSV 
+using DataFrames
 
-# --- REPL (Graphics) stuff ---
-# Colors - printstyled is also possible
+global SETTINGS = Dict() # Settings to import from csv file
+global BUFFER	# Keyboard input handling
+global const GRID = Array{Int}
+
 struct ANSIColor
     red::Function
     bright_red::Function
@@ -51,8 +54,14 @@ struct ANSIColor
     )
 end
 global const ANSI = ANSIColor()
-
 # Keyboard Input
+global const KEY_ESC = "ESC"
+global const KEY_ENTER = "Enter"
+global const KEY_Q = "q"
+global const KEY_Z = "z"
+global const KEY_S = "s"
+global const KEY_D = "d"
+global const KEY_R = "r"
 global const esc_codes = Dict([
     "[A" => "Up",
     "[B" => "Down",
@@ -123,7 +132,55 @@ global const ctrl_codes = Dict([
     30 => "Ctrl-6",
     31 => "Ctrl-7"
 ])
-global BUFFER
+
+# Settings handling
+function ImportSettings()
+    d = Dict()
+	filename = joinpath(@__DIR__, "DotsAndBoxesSettings.csv")
+	if isfile(filename)
+        df = CSV.read(filename, DataFrame)
+        for i in 1:size(df, 1)
+            if !(df[i, 1] in keys(d))
+                d[df[i,1]] = df[i, 2]
+            end
+        end
+        return d
+	else
+    	CreateSettings(filename)
+        df = CSV.read(filename, DataFrame)
+        for i in 1:size(df, 1)
+            if !(df[i, 1] in keys(d))
+                d[df[i,1]] = df[i, 2]
+            end
+        end
+        return d
+	end
+end
+function CreateSettings(filename::String)
+    BaseSettings = Dict([
+    "GRID_WIDTH" => 4,
+    "GRID_HEIGHT" => 4,
+    "PLAYERSIGN1" => "Player1",
+    "PLAYERSIGN2" => "Player2",
+    "STARTCO" => [1, 2],
+    "SPACINGX" => 16,
+    "GRIDPOINT" => "+",
+    "HORZLINE" => "-",
+    "VERTLINE" => "|",
+    "CURSORCHAR" => '0'
+    ])
+    basesett = DataFrame( Setting = ["GRID_WIDTH", "GRID_HEIGHT", "PLAYERSIGN1", "PLAYERSIGN2", "STARTCO", "SPACINGX", "GRIDPOINT", "HORZLINE", "VERTLINE", "CURSORCHAR"],
+                        Value = [BaseSettings["GRID_WIDTH"], BaseSettings["GRID_HEIGHT"], BaseSettings["PLAYERSIGN1"], BaseSettings["PLAYERSIGN2"], BaseSettings["STARTCO"], BaseSettings["SPACINGX"], BaseSettings["GRIDPOINT"], BaseSettings["HORZLINE"], BaseSettings["VERTLINE"], BaseSettings["CURSORCHAR"]])
+    CSV.write(filename, basesett)
+end
+function fixsettings()
+	global SETTINGS
+	SETTINGS["SPACINGX"] = parse(Int, SETTINGS["SPACINGX"])
+	#SETTINGS["CURSORCHAR"] = parse(Char, SETTINGS["CURSORCHAR"])
+	#SETTINGS["STARTCO"] = parse(Array, SETTINGS["STARTCO"])
+	SETTINGS["CURSORCHAR"] = '0'
+	SETTINGS["STARTCO"] = [1, 2]
+end
 function Initiate_Keyboard_Input()
 	# https://discourse.julialang.org/t/wait-for-a-keypress/20218/4
 	# https://docs.julialang.org/en/v1/manual/calling-c-and-fortran-code/#Calling-C-and-Fortran-Code
@@ -136,7 +193,6 @@ function Initiate_Keyboard_Input()
     end
 end
 function readinput()
-	# TODO: problem when esc is hit -> SOLVED, but needs to be pressed twice
 	global BUFFER
 	global ctrl_codes
 	global esc_codes
@@ -147,7 +203,7 @@ function readinput()
        		while isempty(esc_s) || !(esc_s[end] in ['A','B','C','D','F','H','P','Q','R','S','~', '\e'])
             	esc_s *= take!(BUFFER)
         	end
-       	 	if haskey(esc_codes, esc_s) # Check whether key is in dictionary or not
+       	 	if haskey(esc_codes, esc_s)
        	 		return esc_codes[esc_s]
        	 	end
     	elseif Int(s) in 0:31
@@ -157,15 +213,7 @@ function readinput()
     	end
     end
 end
-# Keys
-global const KEY_ESC = "ESC"
-global const KEY_ENTER = "Enter"
-global const KEY_Q = "q"
-global const KEY_Z = "z"
-global const KEY_S = "s"
-global const KEY_D = "d"
-global const KEY_R = "r"
-
+# REPL Graphics Stuff
 # Cursor
 mutable struct CursorStruct
 	x::Int 
@@ -174,31 +222,25 @@ mutable struct CursorStruct
 end
 hidecursor() = print("\e[?25l") # Or \x1b[?25l
 showcursor() = println("\e[?25h")
-
 function clearscreen()
 	# Move cursor to beginning, then clear to end then again begin
+	#println("\x1b[42m")
 	println("\33[H")
 	println("\33[J")
 	println("\33[H")
 	hidecursor()
 end
 
-
-#######################<---After choice--->#######################
-# Functions available to all, both GameZero and REPL
-global const GRID_WIDTH = 4
-global const GRID_HEIGHT = 4
-global const GRID = Array{Int}
-
+########<---- Game Logic and stuff ---->#############
 mutable struct GameState
 	gw::Int # Grid width
 	gh::Int # Grid height
 	grid::GRID # Grid itself
 	gameover::Bool
-	player::Int # Player turn
-	score::Array
-	GameState(	gw = GRID_WIDTH,
-				gh = GRID_HEIGHT,
+	player::Int # Player turn, 1 or 2
+	score::Array # Score[playerone, playertwo]
+	GameState(	gw = parse(Int, SETTINGS["GRID_WIDTH"]),
+				gh = parse(Int, SETTINGS["GRID_HEIGHT"]),
 				grid = resetGrid(gw, gh),
 				gameover = false,
 				player = 1,
@@ -333,17 +375,21 @@ function Difference(state::GameState, oldgrid::GRID)
 	return boxes
 end
 
+function Bot()
+
+end
 #######################<---REPL GAME MODE--->#######################
 function REPLMODE()
+	global SETTINGS
 	# TODO: Make Layout struct
-	spacingx = 10
-	#spacingy = 3
 	startco = [1, 2]
-	GRIDPOINT = ["+", "██"]
-	HORZLINE = ["-", "="]
-	VERTLINE = ["|"]
+	spacingx = 16
+	#spacingy = 3
+	GRIDPOINT = "+" # "██"
+	HORZLINE = "-" # "="
+	VERTLINE = "|"
 	CURSORCHAR = '0'
-	PLAYERSIGN = ["1", "2"]
+	PLAYERSIGN = ["Player1", "Player2"]
 
 	# Move cursor to discrete places, then return string with cursor to print
 	function CursorInGameMove(state::GameState, cursor::CursorStruct, strgrid::Array)
@@ -367,14 +413,14 @@ function REPLMODE()
 		element = collect(element)
 		if cursor.y%2 != 0 # Uneven lines
 			for i in 1:length(element)
-				if string(element[i]) == HORZLINE[1]
-					element[i] = CURSORCHAR
+				if string(element[i]) == SETTINGS["HORZLINE"]
+					element[i] = SETTINGS["CURSORCHAR"]
 				end
 			end
 		else
 			for i in 1:length(element)
-				if string(element[i]) == VERTLINE[1]
-					element[i] = CURSORCHAR
+				if string(element[i]) == SETTINGS["VERTLINE"]
+					element[i] = SETTINGS["CURSORCHAR"]
 				end
 			end
 		end
@@ -392,54 +438,50 @@ function REPLMODE()
         	if y%2 != 0 # Uneven lines (with dots)
 	            for x in 1:size(state.grid, 2)
 	                if state.grid[y, x] == -1
-	                    gridprint[y, x] = "\x1b[$(coy);$(cox)H"*GRIDPOINT[1]
-	                    cox += length(GRIDPOINT[1])
-	                elseif state.grid[y, x] == -2 
-	                    gridprint[y, x] = "\x1b[$(coy);$(cox)H"*" "
-	                    cox += length(" ")
-	                elseif state.grid[y, x] == 10
-	                	gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.red(PLAYERSIGN[1])
-	                	cox += length(PLAYERSIGN[1])
-	                elseif state.grid[y, x] == 20
-	                	gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.cyan(PLAYERSIGN[2])
-	                	cox += length(PLAYERSIGN[2])
+	                    gridprint[y, x] = "\x1b[$(coy);$(cox)H"*SETTINGS["GRIDPOINT"]
+	                    cox += length(SETTINGS["GRIDPOINT"])
 	                elseif state.grid[y,x] == 1 # Player 1 -> Red
-	                    gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.red(HORZLINE[1])^spacingx
-	                    cox += length((HORZLINE[1])^spacingx)
+	                    gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.red(SETTINGS["HORZLINE"])^SETTINGS["SPACINGX"]
+	                    cox += length(SETTINGS["HORZLINE"]^SETTINGS["SPACINGX"])
 	                elseif state.grid[y,x] == 2 # Player 2 -> Blue
-	                    gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.cyan(HORZLINE[1])^spacingx
-	                    cox += length((HORZLINE[1])^spacingx)
+	                    gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.cyan(SETTINGS["HORZLINE"])^SETTINGS["SPACINGX"]
+	                    cox += length(SETTINGS["HORZLINE"]^SETTINGS["SPACINGX"])
 	                elseif state.grid[y,x] == 0 || state.grid[y,x] == 8 # No one -> Colorless
-                    	gridprint[y, x] = "\x1b[$(coy);$(cox)H"*HORZLINE[1]^spacingx
-                    	cox += length(HORZLINE[1]^spacingx)
+                    	gridprint[y, x] = "\x1b[$(coy);$(cox)H"*HORZLINE^SETTINGS["SPACINGX"]
+                    	cox += length(SETTINGS["HORZLINE"]^SETTINGS["SPACINGX"])
 	                end
 	            end
         	else # Even lines
-        		for i in 1:round(spacingx/2-1)
+        		for i in 1:round(SETTINGS["SPACINGX"]/2-1)
         			y = Int(y) # Otherwise errors
         			coy+=1
+        			middlex = Int(round(SETTINGS["SPACINGX"]/2-1))
+        			middley = round(round(SETTINGS["SPACINGX"]/2-1)/2)
 	        		for x in 1:size(state.grid, 2)
-		                if state.grid[y, x] == -1
-		                    gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*GRIDPOINT[1]
-		                    cox += length(GRIDPOINT[1])
-		                elseif state.grid[y, x] == -2
+		                if state.grid[y, x] == -2
 		                   	gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*" "
 		                    cox += length(" ")
 		                elseif state.grid[y, x] == 10
-		                	gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.red(PLAYERSIGN[1])
-		                	cox += length(PLAYERSIGN[1])
+		                	middlesign = Int(round(length(SETTINGS["PLAYERSIGN1"])/2))
+		                	if i == middley
+		                		gridprint[y, x] = "\x1b[$(coy);$(cox-middlex-middlesign)H"*ANSI.red(SETTINGS["PLAYERSIGN1"])
+		                	end
+		                	cox += 1
 	                	elseif state.grid[y, x] == 20
-	                		gridprint[y, x] = "\x1b[$(coy);$(cox)H"*ANSI.cyan(PLAYERSIGN[2])
-		                	cox += length(PLAYERSIGN[2])
+	                		middlesign = Int(round(length(SETTINGS["PLAYERSIGN2"])/2))
+	                		if i == middley
+	                			gridprint[y, x] = "\x1b[$(coy);$(cox-middlex-middlesign)H"*ANSI.cyan(SETTINGS["PLAYERSIGN2"])
+	                		end
+		                	cox += 1
 		                elseif state.grid[y,x] == 1
-		                    gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*ANSI.red(VERTLINE[1])
-		                    cox += spacingx
+		                    gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*ANSI.red(SETTINGS["VERTLINE"])
+		                    cox += SETTINGS["SPACINGX"]
 		                elseif state.grid[y,x] == 2
-		                    gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*ANSI.cyan(VERTLINE[1])
-		                    cox += spacingx
+		                    gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*ANSI.cyan(SETTINGS["VERTLINE"])
+		                    cox += SETTINGS["SPACINGX"]
 		                elseif state.grid[y,x] == 0 || state.grid[y,x] == 8
-	                    	gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*VERTLINE[1]
-	                    	cox += spacingx
+	                    	gridprint[y, x] *= "\x1b[$(coy);$(cox)H"*SETTINGS["VERTLINE"]
+	                    	cox += SETTINGS["SPACINGX"]
 		                end
 		            end
 		            cox = co[1]
@@ -453,7 +495,7 @@ function REPLMODE()
 
 	function PrintInformation(state::GameState, printgrid::Array)
 		a = printgrid[1, end] # Take out the cross element on top row to get coordinate
-		a *= "\x1b[10C" # Move cursor forward by 10
+		a *= "\x1b[10C" # Move cursor forward by 10 spaces
 		if state.player == 1
 				println(a*ANSI.red("Player 1's turn"))
 		elseif state.player == 2
@@ -474,21 +516,21 @@ function REPLMODE()
 			key = readinput()
 
 			if printer == true
-				print(ANSI.yellow("HELP MENU"))
+				println(ANSI.yellow("HELP MENU"))
 				println()
 				println("Movement")
 				println("	Press Z or Up to move the cursor up")
 				println("	Press S or Down to move the cursor down")
 				println("	Press Q or Left to move the cursor to the left")
 				println("	Press D or Right to move the cursor to the right")
-				println("	Press ENTER to color a line")
+				println("	Press ENTER to cross a line")
 				println("Other")
-				println("	Press F2 for settings menu")
-				println("	Press Ctrl-R to reload the game")
-				println("	Press Ctrl-L to reload the screen")
+				println("	Press F2 to access the settings menu")
+				println("	Press Ctrl-L to refresh the screen")
+				println("	Press Ctrl-R to restart the game")
 				println("	Press Ctrl-C to exit game")
 				println()
-				println("	Press ? or F1 to return to the game")
+				println(ANSI.yellow("Press ? or F1 to return to the game"))
 				printer = false
 			end
 			if key == "?" || key == "F1"
@@ -497,35 +539,16 @@ function REPLMODE()
 		end
 	end
 
-	function Settings(state::GameState)
-		clearscreen()
-		change::Bool = false
-		printer::Bool = true
+	function SettingsREPL(state::GameState)
+		# More beautiful and intuitive -> more code
 		while true
 			sleep(0.05)
 			key = readinput()
-			if key == "1"
-				change = true
-				state.gw += 1
-				printer = true
-			elseif key == "2"
-				change = true
-				state.gh += 1
-				printer = true
-			elseif key == "F2"
-				if change
-					return true
-				else
-					return false
-				end
-			end
-			if printer == true
-				clearscreen()
-				println(ANSI.yellow("SETTINGS"))
-				println()
-				println("1: ", "Grid width = $(state.gw)") #TODO, but then state has to be reloaded.
-				println("2: ", "Grid height = $(state.gh)")
-				printer = false
+
+			SETTINGS = ImportSettings()
+			if key == "F2"
+				fixsettings()
+				break
 			end
 		end
 	end
@@ -549,7 +572,7 @@ function REPLMODE()
 
 	# Debugging
 	function printarray(a::Array, state::GameState)
-		for i in 1:(round(spacingx/2-1)*(state.gh-1)+state.gh+1-4)
+		for i in 1:(round(SETTINGS["SPACINGX"]/2-1)*(state.gh-1)+state.gh+1-4)
 			println()
 		end
 		show(stdout, "text/plain", a)
@@ -557,6 +580,9 @@ function REPLMODE()
 
 	# Game loop function
 	function DotsAndBoxesREPL()
+		global SETTINGS
+		SETTINGS = ImportSettings()
+		fixsettings()
 		state::GameState = GameState()
 
 		UPDATE::Bool = true 	# Only update screen if something has happened (ie key press)
@@ -567,16 +593,16 @@ function REPLMODE()
 		cursor = CursorStruct(2, 1)
 
 		prevgrid = state.grid[:,:] # To check for changes
-		printstrgrid = GridToPrint(state, startco)
+		printstrgrid = GridToPrint(state, SETTINGS["STARTCO"])
 		printstrcursor = CursorInGameMove(state, cursor, printstrgrid)
 
-		 # Clear entire console screen
+		 # Clear entire console screen (top line has to be cleared by user in beginning)
     	clearscreen()
     	
     	# Game loop
 		while !state.gameover
 			sleep(0.05)
-			printstrgrid = GridToPrint(state, startco)
+			printstrgrid = GridToPrint(state, SETTINGS["STARTCO"])
 
 			# Key input
 			key = readinput()
@@ -585,18 +611,17 @@ function REPLMODE()
 				RESTART = true
 			elseif key == "Ctrl-C"
 				state.gameover = true
-			elseif key == "Ctrl-L"
+			elseif key == "Ctrl-L" # Refresh
 				UPDATE = true
 			elseif key == "?" || key == "F1"
 				HelpMenu()
 				UPDATE = true
 			elseif key == "F2"
-				if Settings(state)
-					state.grid = resetGrid(state.gw, state.gh)
-					InitiateGrid(state)
-					printstrgrid = GridToPrint(state, startco)
-					printstrcursor = CursorInGameMove(state, cursor, printstrgrid)
-				end
+				SettingsREPL(state)
+				state = GameState()
+				InitiateGrid(state)
+				printstrgrid = GridToPrint(state, SETTINGS["STARTCO"])
+				printstrcursor = CursorInGameMove(state, cursor, printstrgrid)
 				UPDATE = true
 			elseif key == KEY_Z || key == "Up"
 				UPDATE = true
@@ -622,6 +647,7 @@ function REPLMODE()
 					state.grid[cursor.y,cursor.x] = state.player
 
 					boxes = Difference(state, prevgrid)	# Give box to the player who took it
+					checkAround(state)
 
 					# Update the score
 					state.score[state.player] += boxes
@@ -630,7 +656,7 @@ function REPLMODE()
 					cursor.x = 2
 					cursor.y = 1
 
-					printstrgrid = GridToPrint(state, startco)	# Update the grid to later print here, so that the function on next line can take the right information		
+					printstrgrid = GridToPrint(state, SETTINGS["STARTCO"])	# Update the grid (to later print) here, so that the function on next line can take the right information		
 					printstrcursor = CursorInGameMove(state, cursor, printstrgrid)
 
 					# Change turns, if no box is completed
@@ -651,10 +677,11 @@ function REPLMODE()
 					clearscreen()
 					EndGame(state)
 					UPDATE = false
-					continue
+					continue # makes us stay in while loop (state.gameover is still false), in order to use key input
 				end
 				if !state.gameover
 					clearscreen()
+					printstrgrid = GridToPrint(state, SETTINGS["STARTCO"])
 					PrintInformation(state, printstrgrid)
 
 					# Print Grid
@@ -671,7 +698,6 @@ function REPLMODE()
 					# Print Cursor
 					println(printstrcursor)
 
-					checkAround(state) # Can also be placed in elseif enter
 					prevgrid = state.grid[:,:]
 					UPDATE = false
 				end
